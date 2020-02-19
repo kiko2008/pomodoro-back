1a886c5deb60b0e433b8da0c1dce37130e585d05
francisco <francisco.fernandez@papyrum.com>
2020-02-19 16:58:19 +0100

    Add list endpoint for get tasks for user_id

--- pomodoro/api.py
+++ pomodoro/api.py
@@ -1,5 +1,6 @@
 from pomodoro.models import PomodoroConf, PomodoroTask
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
 
 from users.permissions import UserPermission
 from pomodoro.serializer import ConfSerializer, TaskSerializer
@@ -11,9 +12,21 @@ class PomodoroConfViewSet(ModelViewSet):
     serializer_class = ConfSerializer
     permission_classes = [UserPermission]
 
+    def retrieve(self, request):
+        username = request.query_params['username']
+        conf_by_user_queryset = PomodoroConf.objects.filter(user_id=2).order_by('-pub_date')
+        serializer = ConfSerializer(conf_by_user_queryset)
+        return Response(serializer.data)
 
 class PomodoroTaskViewSet(ModelViewSet):
 
     queryset = PomodoroTask.objects.all()
     serializer_class = TaskSerializer
     permission_classes = [UserPermission]
+    lookup_field = 'user'
+
+    def list(self, request):
+        username = request.query_params['username']
+        tasks_by_user_queryset = PomodoroTask.objects.filter(user=username).order_by('-pub_date')
+        serializer = TaskSerializer(tasks_by_user_queryset, many=True)
+        return Response(serializer.data)
